a57426431234eac0e47130c74688f0bb40467884
Abhay Thakur <abhaysinghthakur112212@gmail.com>
2025-07-29 23:27:12 +0530

    Updated frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8089',
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8089', // base URL for all API calls
 });
 
 api.interceptors.request.use((config) => {
